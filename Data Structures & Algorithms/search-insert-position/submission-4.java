d9d66b1c67d348fc0f5df9040f1bc891bf994509
txcallen <allenxietianchong@gmail.com>
2026-04-29 16:04:22 -0400

    Add: search-insert-position - submission-4

---
+++ Data Structures & Algorithms/search-insert-position/submission-4.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int searchInsert(int[] nums, int target) {
+        
+        int l = 0, r = nums.length-1;
+        int res = nums.length;
+        while(l <= r){
+            int mid = l + (r - l) / 2;
+
+            if(nums[mid] == target){
+                return mid;
+            }
+
+            if(nums[mid] > target){
+                res = mid;
+                r = mid - 1;
+            }else{
+                l = mid + 1;
+            }
+        }
+
+        return res;
+        
+    }
+}
