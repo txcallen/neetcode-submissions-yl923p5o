02a619619504c27a473dfe92c75f13a42cf9c5ab
txcallen <allenxietianchong@gmail.com>
2026-05-26 10:49:14 -0400

    Add: online-stock-span - submission-0

---
+++ Data Structures & Algorithms/online-stock-span/submission-0.java
@@ -0,0 +1,23 @@
+class StockSpanner {
+    
+    Deque<int[]> dq = new ArrayDeque<>();
+
+    public StockSpanner() {}
+
+    public int next(int price) {
+        int span = 1;
+        while (!dq.isEmpty() && dq.peek()[0] <= price) {
+            span += dq.pop()[1];
+        }
+
+        dq.push(new int[] {price, span});
+
+        return span;
+    }
+}
+
+/**
+ * Your StockSpanner object will be instantiated and called as such:
+ * StockSpanner obj = new StockSpanner();
+ * int param_1 = obj.next(price);
+ */
